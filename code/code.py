1194d739a6109fda79e48f6eff5c07751caee397
jizhenjun <1569778661@qq.com>
2021-05-04 16:27:01 +0800

    add code

---
+++ code/code.py
@@ -0,0 +1,192 @@
+# import packages
+import time, math, os
+from tqdm import tqdm
+import gc
+import pickle
+import random
+from datetime import datetime
+from operator import itemgetter
+import numpy as np
+import pandas as pd
+import warnings
+from collections import defaultdict
+import collections
+warnings.filterwarnings('ignore')
+
+# debug模式：从训练集中划出一部分数据来调试代码
+def get_all_click_sample(data_path=r'../user_data/tianchi_small/'):
+    """
+        训练集中采样一部分数据调试
+        data_path: 原数据的存储路径
+    """
+    all_click = pd.read_csv(data_path + 'small_click_log.csv')
+
+    all_click = all_click.drop_duplicates((['user_id', 'click_article_id', 'click_timestamp']))
+    return all_click
+
+# 读取点击数据，这里分成线上和线下，如果是为了获取线上提交结果应该讲测试集中的点击数据合并到总的数据中
+# 如果是为了线下验证模型的有效性或者特征的有效性，可以只使用训练集
+def get_all_click_df(data_path=r'../tcdata/', offline=True):
+    if offline:
+        all_click = pd.read_csv(data_path + 'train_click_log.csv')
+    else:
+        trn_click = pd.read_csv(data_path + 'train_click_log.csv')
+        tst_click = pd.read_csv(data_path + 'testA_click_log.csv')
+
+        all_click = trn_click.append(tst_click)
+    
+    all_click = all_click.drop_duplicates((['user_id', 'click_article_id', 'click_timestamp']))
+    return all_click
+
+def get_user_item_time(click_df):
+    
+    click_df = click_df.sort_values('click_timestamp')
+    def make_item_time_pair(df):
+        return list(zip(df['click_article_id'], df['click_timestamp']))
+    user_item_time_df = click_df.groupby('user_id')['click_article_id', 'click_timestamp'].apply(lambda x: make_item_time_pair(x))\
+                                                            .reset_index().rename(columns={0: 'item_time_list'})
+    user_item_time_dict = dict(zip(user_item_time_df['user_id'], user_item_time_df['item_time_list']))
+    return user_item_time_dict
+
+# 获取近期点击最多的文章
+def get_item_topk_click(click_df, k):
+    topk_click = click_df['click_article_id'].value_counts().index[:k]
+    return topk_click
+
+# 获取当前数据的历史点击和最后一次点击
+def get_hist_and_last_click(all_click):
+    all_click = all_click.sort_values(by=['user_id', 'click_timestamp'])
+    click_last_df = all_click.groupby('user_id').tail(1)
+    # 如果用户只有一个点击，hist为空了，会导致训练的时候这个用户不可见，此时默认泄露一下
+    def hist_func(user_df):
+        if len(user_df) == 1:
+            return user_df
+        else:
+            return user_df[:-1]
+    click_hist_df = all_click.groupby('user_id').apply(hist_func).reset_index(drop=True)
+    return click_hist_df, click_last_df
+
+def itemcf_sim(df):
+    """
+        文章与文章之间的相似性矩阵计算
+        :param df: 数据表
+        :item_created_time_dict:  文章创建时间的字典
+        return : 文章与文章的相似性矩阵
+        思路: 基于物品的协同过滤(详细请参考上一期推荐系统基础的组队学习)， 在多路召回部分会加上关联规则的召回策略
+    """
+    
+    user_item_time_dict = get_user_item_time(df)
+    
+    # 计算物品相似度
+    i2i_sim = {}
+    item_cnt = defaultdict(int)
+    for user, item_time_list in tqdm(user_item_time_dict.items()):
+        # 在基于商品的协同过滤优化的时候可以考虑时间因素
+        for i, i_click_time in item_time_list:
+            item_cnt[i] += 1
+            i2i_sim.setdefault(i, {})
+            for j, j_click_time in item_time_list:
+                if(i == j):
+                    continue
+                i2i_sim[i].setdefault(j, 0)
+                i2i_sim[i][j] += 1 / math.log(len(item_time_list) + 1)
+                
+    i2i_sim_ = i2i_sim.copy()
+    for i, related_items in i2i_sim.items():
+        for j, wij in related_items.items():
+            i2i_sim_[i][j] = wij / math.sqrt(item_cnt[i] * item_cnt[j])
+    
+    # 将得到的相似性矩阵保存到本地
+    # pickle.dump(i2i_sim_, open(save_path + 'itemcf_i2i_sim.pkl', 'wb'))
+    
+    return i2i_sim_
+
+def item_based_recommend(user_id, user_item_time_dict, i2i_sim, sim_item_topk, recall_item_num, item_topk_click):
+    """
+        基于文章协同过滤的召回
+        :param user_id: 用户id
+        :param user_item_time_dict: 字典, 根据点击时间获取用户的点击文章序列   {user1: [(item1, time1), (item2, time2)..]...}
+        :param i2i_sim: 字典，文章相似性矩阵
+        :param sim_item_topk: 整数， 选择与当前文章最相似的前k篇文章
+        :param recall_item_num: 整数， 最后的召回文章数量
+        :param item_topk_click: 列表，点击次数最多的文章列表，用户召回补全        
+        return: 召回的文章列表 {item1:score1, item2: score2...}
+        注意: 基于物品的协同过滤(详细请参考上一期推荐系统基础的组队学习)， 在多路召回部分会加上关联规则的召回策略
+    """
+    
+    # 获取用户历史交互的文章
+    user_hist_items = user_item_time_dict[user_id]
+    user_hist_items_ = {user_id for user_id, _ in user_hist_items}
+    
+    item_rank = {}
+    for loc, (i, click_time) in enumerate(user_hist_items):
+        for j, wij in sorted(i2i_sim[i].items(), key=lambda x: x[1], reverse=True)[:sim_item_topk]:
+            if j in user_hist_items_:
+                continue
+                
+            item_rank.setdefault(j, 0)
+            item_rank[j] +=  wij
+    
+    # 不足10个，用热门商品补全
+    if len(item_rank) < recall_item_num:
+        for i, item in enumerate(item_topk_click):
+            if item in item_rank.items(): # 填充的item应该不在原来的列表中
+                continue
+            item_rank[item] = - i - 100 # 随便给个负数就行
+            if len(item_rank) == recall_item_num:
+                break
+    
+    item_rank = sorted(item_rank.items(), key=lambda x: x[1], reverse=True)[:recall_item_num]
+        
+    return item_rank
+
+# 依次评估召回的前10, 20, 30, 40, 50个文章中的击中率
+def metrics_recall(user_recall_items_dict, trn_last_click_df, topk=5):
+    last_click_item_dict = dict(zip(trn_last_click_df['user_id'], trn_last_click_df['click_article_id']))
+    user_num = len(user_recall_items_dict)
+    for k in range(10, topk+1, 10):
+        hit_num = 0
+        for user, item_list in user_recall_items_dict.items():
+            # 获取前k个召回的结果
+            tmp_recall_items = [x[0] for x in user_recall_items_dict[user][:k]]
+            if last_click_item_dict[user] in set(tmp_recall_items):
+                hit_num += 1
+        
+        hit_rate = round(hit_num * 1.0 / user_num, 5)
+        print(' topk: ', k, ' : ', 'hit_num: ', hit_num, 'hit_rate: ', hit_rate, 'user_num : ', user_num)
+
+if __name__ == '__main__':
+    all_click_df = get_all_click_df()
+    # 提取最后一次点击作为召回评估，如果不需要做召回评估直接使用全量的训练集进行召回(线下验证模型)
+    # 如果不是召回评估，直接使用全量数据进行召回，不用将最后一次提取出来
+    all_click_df, trn_last_click_df = get_hist_and_last_click(all_click_df)
+    i2i_sim = itemcf_sim(all_click_df)
+    # 定义
+    user_recall_items_dict = collections.defaultdict(dict)
+    # 获取 用户 - 文章 - 点击时间的字典*
+    user_item_time_dict = get_user_item_time(all_click_df)
+    # 去取文章相似度
+    # i2i_sim = pickle.load(open(save_path + 'itemcf_i2i_sim.pkl', 'rb'))
+    # 相似文章的数量
+    sim_item_topk = 10
+    # 召回文章数量
+    recall_item_num = 10
+    # 用户热度补全
+    item_topk_click = get_item_topk_click(all_click_df, k=0)
+
+    for user in tqdm(all_click_df['user_id'].unique()):
+        user_recall_items_dict[user] = item_based_recommend(user, user_item_time_dict, i2i_sim, 
+                                                            sim_item_topk, recall_item_num, item_topk_click)
+    
+    metrics_recall(user_recall_items_dict, trn_last_click_df, topk=10)
+
+    # 将字典的形式转换成df
+    user_item_score_list = []
+
+    for user, items in tqdm(user_recall_items_dict.items()):
+        for item, score in items:
+            user_item_score_list.append([user, item, score])
+
+    recall_df = pd.DataFrame(user_item_score_list, columns=['user_id', 'click_article_id', 'pred_score'])
+
+    # print(trn_last_click_df)
